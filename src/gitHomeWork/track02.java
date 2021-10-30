608f28e5feae03c6a38a7370b5251a9492e08e86
mehmetozturkeri <eminozturk02@gmail.com>
2021-10-30 17:10:52 -0500

    saving gitHomeWork

---
+++ src/gitHomeWork/track02.java
@@ -0,0 +1,10 @@
+package gitHomeWork;
+
+public class track02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello Ozturkeri");
+	}
+
+}
---
+++ src/gitHomeWork/track1.java
@@ -0,0 +1,11 @@
+
+package gitHomeWork;
+
+public class track1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello Mehmet");
+	}
+
+}
